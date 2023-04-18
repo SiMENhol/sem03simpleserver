9b9185e3ce3e7f92b08bdb2516d2381ddcdac3fa
Simen <simenhol@uia.no>
2023-04-18 13:25:54 +0200

    hei

--- tcp_server.go
+++ tcp_server.go
@@ -5,10 +5,11 @@ import (
 	"log"
 	"net"
 	"sync"
+
+	"github.com/SiMENhol/is105sem03/mycrypt"
 )
 
 func main() {
-
 	var wg sync.WaitGroup
 
 	server, err := net.Listen("tcp", "172.17.0.2:8080")
@@ -23,7 +24,8 @@ func main() {
 			log.Println("før server.Accept() kallet")
 			conn, err := server.Accept()
 			if err != nil {
-				return
+				log.Println(err)
+
 			}
 			go func(c net.Conn) {
 				defer c.Close()
@@ -34,23 +36,38 @@ func main() {
 						if err != io.EOF {
 							log.Println(err)
 						}
-						return // fra for løkke
+						return // fra for-løkke
 					}
-					switch msg := string(buf[:n]); msg {
+
+					// Dekrypterer meldingen
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekryptert melding: ", string(dekryptertMelding))
+
+					// Sjekk om dekrypteringen var vellykket
+					if err != nil {
+						log.Println("Dekryptering feilet: ", err)
+						return // fra for-løkke
+					}
+
+					// Behandle den dekrypterte meldingen
+					switch msg := string(dekryptertMelding); msg {
 					case "ping":
 						_, err = c.Write([]byte("pong"))
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
+						if err != nil {
 							log.Println(err)
+							return // fra for-løkke
 						}
-						return // fra for løkke
+					default:
+						_, err = c.Write([]byte("Ukjent melding"))
+						if err != nil {
+							log.Println(err)
+							return // fra for-løkke
+						}
 					}
 				}
 			}(conn)
 		}
 	}()
+
 	wg.Wait()
 }
