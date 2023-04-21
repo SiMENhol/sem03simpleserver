71166952923c6165b15df864493cc85e6d16f50a
Simen <simenhol@uia.no>
2023-04-21 12:14:54 +0200

    aSD

--- tcp_server.go
+++ tcp_server.go
@@ -20,6 +20,81 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer server.Close()
+
+	log.Printf("bundet til %s", server.Addr().String())
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for {
+			log.Println("før server.Accept() kallet")
+
+			conn, err := server.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			wg.Add(1)
+			go func(c net.Conn) {
+				defer wg.Done()
+				defer conn.Close()
+
+				for {
+					buf := make([]byte, 2048)
+					n, err := c.Read(buf)
+					if err != nil {
+						if err != io.EOF {
+							log.Println(err)
+						}
+						return
+					}
+
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
+					msgString := string(dekryptertMelding)
+
+					switch msgString {
+					case "ping":
+						kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+						log.Println("Kryptert melding: ", string(kryptertMelding))
+						_, err = c.Write([]byte(string(kryptertMelding)))
+					case "Kjevik":
+						parts := strings.Split(msgString, " ")
+						celsius, _ := strconv.ParseFloat(parts[1], 64)
+						fahrenheit := conv.CelsiusToFahrenheit(celsius)
+						kryptertMelding := mycrypt.Krypter([]rune(fmt.Sprintf("Temperaturen i Fahrenheit er %.2f", fahrenheit)), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+						log.Println("Kryptert melding: ", string(kryptertMelding))
+						_, err = c.Write([]byte(string(kryptertMelding)))
+					default:
+						_, err = c.Write(buf[:n])
+					}
+
+					if err != nil {
+						if err != io.EOF {
+							log.Println(err)
+						}
+						return
+					}
+				}
+			}(conn)
+		}
+	}()
+
+	wg.Wait()
+}
+
+/*
+func main() {
+	var wg sync.WaitGroup
+
+	server, err := net.Listen("tcp", "172.17.0.3:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Printf("bundet til %s", server.Addr().String())
 	wg.Add(1)
 	go func() {
@@ -80,3 +155,4 @@ func main() {
 
 	wg.Wait()
 }
+*/
